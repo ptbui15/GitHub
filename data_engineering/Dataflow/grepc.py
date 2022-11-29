d8063b1fbe86d98552465988e250bced7b7dad7d
ptbui15 <94770068+ptbui15@users.noreply.github.com>
2022-11-29 15:28:26 -0500

    run tensorflow

--- data_engineering/Dataflow/grepc.py
+++ data_engineering/Dataflow/grepc.py
@@ -29,7 +29,7 @@ def run():
       '--save_main_session',
       '--staging_location=gs://{0}/staging/'.format(BUCKET),
       '--temp_location=gs://{0}/staging/'.format(BUCKET),
-      '--region=us-central1',
+      '--region=us-west1',
       '--runner=DataflowRunner'
    ]
 
